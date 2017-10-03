02e3b870357f34fecfa590719e5d843f22cb3ce8
MikeDomingues <mikedomingues@greywolf-entertainment.com>
2017-10-03 17:32:49 +0100

    Revert "Revert "MyChange""
    This reverts commit 47096752adcf98f43ba5d29a617e24d600b858d9.

---
+++ Assets/move.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class move : MonoBehaviour {
+	public float speed = 5;
+
+	// Update is called once per frame
+	void Update () {
+		this.gameObject.transform.Translate (Vector3.right * speed * Time.deltaTime);
+	}
+}
