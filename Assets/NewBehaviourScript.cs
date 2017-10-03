a7a90898ac55acd3738390f0f2e226289b9c0cc7
MikeDomingues <mikedomingues@greywolf-entertainment.com>
2017-10-03 16:56:18 +0100

    hh

--- Assets/NewBehaviourScript.cs
+++ Assets/NewBehaviourScript.cs
@@ -4,12 +4,13 @@ using System.Collections;
 public class NewBehaviourScript : MonoBehaviour {
 
 	// Use this for initialization
-	void Start () {
-	
-	}
+
 	
 	// Update is called once per frame
 	void Update () {
-	
+        if (0 == 1)
+            {
+
+            }
 	}
 }
